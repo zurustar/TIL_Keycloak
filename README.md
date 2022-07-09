9dfa1694469b3d87b3c004ffb39a33cf510b6623
zurustar <zurustar@users.noreply.github.com>
2022-07-09 17:45:39 +0900

    updated

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # TIL_Keycloak
+
+## Keycloak の起動
+
+Keycloak を起動する。[https://www.keycloak.org/getting-started/getting-started-docker](こちら)に従って Docker でやってみる。
+
+ちなみに keycloak のバージョン 17 から起動方法が変わっているので注意。
+
+```
+docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:18.0.2 start-dev
+```
+
+これで、ブラウザで http://localhost:8080/にアクセスすると keycloak の Web インタフェースにアクセスすることができる。上記コマンドをよく見るとわかるように、管理者のアカウントとパスワードは両方とも admin になっている。
+
+## 実験用クライアントの環境準備
+
+手元のマシンに Python が入っていたので Python でやってみる。
+一応仮想環境をつくってから実施する。
+
+```
+python -m venv .venv
+```
+
+有効化するコマンドは OS によって異なる。以下は Windows の場合。
+
+```
+.\.venv\Scripts\activate.bat
+```
+
+あとはコードを見てください。レルム一覧取得、レルム削除、レルム作成、レルムロール作成、レルムロール情報取得、グループ作成、ユーザ作成、ユーザ情報取得、ユーザのグループへの追加を実行しています。
+ユーザ作成時に直接ロールに追加できるのではないだろうか？と疑っています。
---
+++ client.py
@@ -0,0 +1,175 @@
+
+import requests
+import json
+import csv
+
+url = 'http://localhost:8080'
+my_realm = 'jikken'
+my_roles =  ["jikken_user", "jikken_superuser", "jikken_administrator"]
+my_groups =  ["jikken_teamA", "jikken_teamB", "jikken_teamC", "jikken_teamD"]
+
+csv_file = './dummy.csv'
+
+#
+# アクセストークンを取得
+#
+def get_access_token():
+    print("get_access_token()")
+    r = requests.post(
+        url+'/realms/master/protocol/openid-connect/token',
+        data={'username':'admin', 'password':'admin', 'grant_type': 'password', 'client_id':'admin-cli'})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+    j = json.loads(r.text)
+    return j['access_token']
+
+#
+# いま設定されているレルムを取得してみる
+#
+def get_realms(token):
+    print("get_realms("+token+")")
+    r = requests.get(url+'/admin/realms',headers={'Authorization': 'Bearer ' + token})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+    return json.loads(r.text)
+
+#
+# いまから作ろうとしているレルムがすでにあったら消してみる
+#
+def del_realms(token, realm):
+    print("del_realms("+token+", "+realm+")")
+    r = requests.delete(url+'/admin/realms/' + realm, headers={'Authorization': 'Bearer ' +token})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+
+#
+# レルム作成
+#
+def create_realm(token, realm):
+    print("create_realms("+token+", "+realm+")")
+    r = requests.post(
+        url + '/admin/realms',
+        headers={'Authorization': 'Bearer ' + token},
+        json={'realm': realm, 'enabled': True})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+
+#
+# ロール作成
+#
+def create_role(token, realm, role):
+    print("create_role("+token+", "+realm+", "+role+")")
+    r = requests.post(
+        url+'/admin/realms/' + realm + '/roles',
+        headers={'Authorization': 'Bearer ' + token},
+        json={'name': role})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+
+#
+# ロール情報取得
+#
+def get_roles_info(token, realm):
+    print("get_roles_info("+token+", "+realm+")")
+    r = requests.get(
+        url+'/admin/realms/' + realm + '/roles',
+        headers={'Authorization': 'Bearer ' + token})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+    result = {}
+    for role in json.loads(r.text):
+        result[role['name']]=role
+    return result
+
+#
+# グループを作る
+#
+def create_group(token, realm, group):
+    print("create_group("+token+", "+realm+", "+group+")")
+    r = requests.post(
+        url+'/admin/realms/' + realm + '/groups',
+        headers={'Authorization': 'Bearer ' + token},
+        json={'name': group})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        quit()
+
+#
+# ユーザを作成
+#
+def create_user(token, realm, user, group):
+    print("create_user("+token+", "+realm+", "+user[0]+", "+group+")")
+    ary = user[1].split(' ')
+    r = requests.post(
+        url+'/admin/realms/' + realm + '/users',
+        headers={'Authorization': 'Bearer ' + token},
+        json={
+            'username': user[0],
+            'email': user[6],
+            'firstName': ary[0],
+            'lastName': ary[1],
+            'groups': [group],
+            'attributes': {"age": user[2], "zipcode": user[9], "address": user[10], "company": user[11]},
+            'enabled': True})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        print(r.text)
+        quit()
+
+
+def get_user_info(token, realm, username):
+    print("get_user_info("+token+", "+realm+", "+username+")")
+    r = requests.get(
+        url+'/admin/realms/' + realm + '/users?username='+username ,
+        headers={'Authorization': 'Bearer ' + token})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        print(r.text)
+        quit()
+    return json.loads(r.text)[0]
+
+def set_user_role(token, realm, uid, role_id, role_name):
+    print("set_user_role("+token+", "+realm+", "+uid+", "+ role_id+", "+role_name+")")
+    r = requests.post(
+        url+'/admin/realms/' + realm + '/users/' + uid + "/role-mappings/realm",
+        headers={'Authorization': 'Bearer ' + token},
+        json=[{'id':role_id,'name': role_name, 'composite': False, 'clientRole': False, 'containerId': ''}])
+    if not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        print(r.text)
+        quit()
+
+
+def main():
+    token = get_access_token()
+    realms = get_realms(token)
+    for realm in realms:
+        if realm['realm'] == my_realm:
+            del_realms(token, my_realm)
+    create_realm(token, my_realm)
+    for role in my_roles:
+        create_role(token, my_realm, role)
+    role_info = get_roles_info(token, my_realm)
+    for g in my_groups:
+        create_group(token, my_realm, g)
+
+
+    #   ダミーのユーザとしてこの素晴らしいサイトで生成したデータを試用する。
+    #     https://testdata.userlocal.jp/
+    text = open(csv_file, "r", encoding="utf-8", errors="", newline="" )
+    f = csv.reader(text, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    header = next(f)
+    for i, user in enumerate(f):
+        create_user(token, my_realm, user, my_groups[i%len(my_groups)])
+        user = get_user_info(token, my_realm, user[0])
+        rolename = my_roles[i%len(my_roles)]
+        set_user_role(token, my_realm, user['id'], role_info[role]['id'], role)
+
+if __name__ == '__main__':
+    main()
