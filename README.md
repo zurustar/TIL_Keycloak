519ee4157d2dc9c62bfe5b2fc153e979279c9807
zurustar <zurustar@users.noreply.github.com>
2022-07-10 18:00:56 +0900

    updated

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 [Keycloak](https://www.keycloak.org/) の基本的な使い方を把握するために試行錯誤している記録です。
 
-規模の大きいシステムでユーザー登録等を GUI で実施するのは現実的でないように思えたので、極力 API で操作しています。
+規模の大きいシステムでユーザー登録等を GUI で実施するのは現実的でないように思えたので、[Keycloak](https://www.keycloak.org/) を操作する際には極力 API を使用しています。
 
 ## Keycloak の起動
 
-[Keycloak](https://www.keycloak.org/) を起動する。[こちら](https://www.keycloak.org/getting-started/getting-started-docker)に従って [Docker](https://www.docker.com/) でやってみる。
-
-ちなみに [Keycloak](https://www.keycloak.org/) のバージョン 17 から起動方法が変わっているので注意。
+[こちら](https://www.keycloak.org/getting-started/getting-started-docker)に従って [Docker](https://www.docker.com/) でやってみる。
 
 ```
 docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:18.0.2 start-dev
@@ -30,17 +28,11 @@ services:
       - start-dev
 ```
 
-これで、ブラウザで http://localhost:8080/ にアクセスすると [Keycloak](https://www.keycloak.org/) の Web インタフェースにアクセスすることができる。上記コマンドをよく見るとわかるように、管理者のアカウントとパスワードは両方とも admin になっている。
+起動が完了したっぽい雰囲気になったら、起動したのと同一のマシンにてブラウザを用いて [http://localhost:8080/](http://localhost:8080/) にアクセスすると [Keycloak](https://www.keycloak.org/) の Web インタフェースを利用できる。上記コマンドをよく見るとわかるように、管理者のアカウントとパスワードは起動時に環境変数で与えていて、両方とも admin になっている。私の PC では 1 万ユーザ分のデータを使って 10 秒強で実行が完了する。
 
-## リバースプロキシの起動
+ちなみに [Keycloak](https://www.keycloak.org/) のバージョン 17 から起動方法が変わっているので注意。これを書いている時点で唯一と思われる日本語書かれた Keycloak の書籍はバージョン 15 を使用しているので、現行バージョンとは違う起動の仕方が説明されている。
 
-SSO を実現する方法のひとつに、Web アプリの前段に [OIDC](https://openid.net/connect/) に対応したリバースプロキシを設置して、認証回りの処理は全てこいつにやらせるという方法がある。
-
-[Apache](https://httpd.apache.org/)では [mod_auth_opendic](https://github.com/zmartzone/mod_auth_openidc) というモジュールがあるので、これを使ってみることにする。
-
-...鋭意実験中。
-
-## 実験用クライアントの環境準備
+## 実験用データの環境準備
 
 手元のマシンに [python](https://www.python.org/) が入っていたので [python](https://www.python.org/) でやってみる。
 一応仮想環境をつくってから実施する。
@@ -49,13 +41,22 @@ SSO を実現する方法のひとつに、Web アプリの前段に [OIDC](http
 python -m venv .venv
 ```
 
-有効化するコマンドは OS によって異なる。以下は [Windows](https://www.microsoft.com/ja-jp/windows/) の場合。
+有効化するコマンドは OS によって異なる。以下は [Windows](https://www.microsoft.com/ja-jp/windows/) で PowerShell を使っている場合。
 
 ```
-.\.venv\Scripts\activate.bat
+.\.venv\Scripts\activate.ps1
 ```
 
-これを実施してから、このリポジトリに含まれる [client.py](https://github.com/zurustar/TIL_Keycloak/blob/main/tool/client.py) を実行してください。
+これを実施してからこのリポジトリに含まれる [client.py](https://github.com/zurustar/TIL_Keycloak/blob/main/tool/client.py) を実行してください。pip install requests しないといけないかもしれません。何をしているかはコードを見てください。レルム一覧取得、レルム削除、レルム作成、レルムロール作成、レルムロール情報取得、グループ作成、ユーザ作成、ユーザ情報取得、ユーザのグループへの追加、クライアントの登録を実行している。
 
-あとはコードを見てください。レルム一覧取得、レルム削除、レルム作成、レルムロール作成、レルムロール情報取得、グループ作成、ユーザ作成、ユーザ情報取得、ユーザのグループへの追加を実行しています。
-ユーザ作成時に直接ロールに追加できるのではないだろうか？と疑っていますが今のところよくわからず、引き続き調査中です。
+ユーザ作成時に直接ロールに追加できるのではないだろうか？と疑っていますが今のところよくわからず、引き続き調査する。
+
+## リバースプロキシの起動
+
+SSO を実現する方法のひとつに、Web アプリの前段に [OIDC](https://openid.net/connect/) に対応したリバースプロキシを設置して、認証回りの処理は全てこいつにやらせるという方法がある。
+
+[Apache](https://httpd.apache.org/)では [mod_auth_opendic](https://github.com/zmartzone/mod_auth_openidc) というモジュールがあるので、これを使ってみることにする。
+
+このリバースプロキシに認証周りの処理を実行してほしいので、クライアントとして [Keycloak](https://www.keycloak.org/)に登録する…というのは実はこの前に実行している[ツール](https://github.com/zurustar/TIL_Keycloak/blob/main/tool/client.py)の中で実施済み。手動で実施する場合は、[Keycloak](https://www.keycloak.org/) の [管理コンソール](http://localhost:8080/) に管理者でログインして左メニューの Clients をクリックして[表示される画面](http://localhost:8080/admin/master/console/#/realms/jikken/clients)で適宜入力すればよい。
+
+※現在リバプロ用の Apache を起動する Dockerfile 作成で試行錯誤中。わかったらまた追記します。
--- tool/client.py
+++ tool/client.py
@@ -2,11 +2,14 @@
 import requests
 import json
 import csv
+import time
 
 url = 'http://localhost:8080'
 my_realm = 'jikken'
 my_roles =  ["jikken_user", "jikken_superuser", "jikken_administrator"]
 my_groups =  ["jikken_teamA", "jikken_teamB", "jikken_teamC", "jikken_teamD"]
+
+my_client = "demo_reverse_proxy"
 
 #   ダミーのユーザとしてこの素晴らしいサイトで生成したデータを試用する。
 #     https://testdata.userlocal.jp/
@@ -16,12 +19,12 @@ csv_file = './dummy.csv'
 # アクセストークンを取得
 #
 def get_access_token():
-    print("get_access_token()")
     r = requests.post(
         url+'/realms/master/protocol/openid-connect/token',
         data={'username':'admin', 'password':'admin', 'grant_type': 'password', 'client_id':'admin-cli'})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
     j = json.loads(r.text)
     return j['access_token']
@@ -30,10 +33,10 @@ def get_access_token():
 # いま設定されているレルムを取得してみる
 #
 def get_realms(token):
-    print("get_realms("+token+")")
     r = requests.get(url+'/admin/realms',headers={'Authorization': 'Bearer ' + token})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
     return json.loads(r.text)
 
@@ -41,73 +44,80 @@ def get_realms(token):
 # いまから作ろうとしているレルムがすでにあったら消してみる
 #
 def del_realms(token, realm):
-    print("del_realms("+token+", "+realm+")")
     r = requests.delete(url+'/admin/realms/' + realm, headers={'Authorization': 'Bearer ' +token})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
 
 #
 # レルム作成
 #
 def create_realm(token, realm):
-    print("create_realms("+token+", "+realm+")")
     r = requests.post(
         url + '/admin/realms',
         headers={'Authorization': 'Bearer ' + token},
         json={'realm': realm, 'enabled': True})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
 
 #
 # ロール作成
 #
 def create_role(token, realm, role):
-    print("create_role("+token+", "+realm+", "+role+")")
+    # とりあえず作る
     r = requests.post(
         url+'/admin/realms/' + realm + '/roles',
         headers={'Authorization': 'Bearer ' + token},
         json={'name': role})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
 
-#
-# ロール情報取得
-#
-def get_roles_info(token, realm):
-    print("get_roles_info("+token+", "+realm+")")
+def get_roles(token, realm):
+    # 作った情報を取得
     r = requests.get(
         url+'/admin/realms/' + realm + '/roles',
         headers={'Authorization': 'Bearer ' + token})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
-    result = {}
-    for role in json.loads(r.text):
-        result[role['name']]=role
-    return result
+    return json.loads(r.text)
 
 #
 # グループを作る
 #
 def create_group(token, realm, group):
-    print("create_group("+token+", "+realm+", "+group+")")
+    # 作る
     r = requests.post(
         url+'/admin/realms/' + realm + '/groups',
         headers={'Authorization': 'Bearer ' + token},
         json={'name': group})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
+        print(r.text)
         quit()
+
+def get_groups(token, realm):
+    # 作ったグループの情報を取得する
+    r = requests.get(
+        url+'/admin/realms/' + realm + '/groups',
+        headers={'Authorization': 'Bearer ' + token})
+    if  not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        print(r.text)
+        quit()
+    return json.loads(r.text)
 
 #
 # ユーザを作成
 #   TODO: 同時にロールを追加したい。
 #
 def create_user(token, realm, user, group):
-    print("create_user("+token+", "+realm+", "+user[0]+", "+group+")")
     ary = user[1].split(' ')
     r = requests.post(
         url+'/admin/realms/' + realm + '/users',
@@ -117,8 +127,12 @@ def create_user(token, realm, user, group):
             'email': user[6],
             'firstName': ary[0],
             'lastName': ary[1],
-            'groups': [group],
-            'attributes': {"age": user[2], "zipcode": user[9], "address": user[10], "company": user[11]},
+            'groups': [group], # グループに登録できる
+            'attributes': {
+                "age": user[2],
+                "zipcode": user[9],
+                "address": user[10],
+                "company": user[11]}, # 任意の情報を保存できる
             'enabled': True})
     if  not (200 <= r.status_code and r.status_code < 300):
         print(r.status_code)
@@ -128,8 +142,7 @@ def create_user(token, realm, user, group):
 #
 # ユーザ情報取得
 #
-def get_user_info(token, realm, username):
-    print("get_user_info("+token+", "+realm+", "+username+")")
+def get_user(token, realm, username):
     r = requests.get(
         url+'/admin/realms/' + realm + '/users?username='+username ,
         headers={'Authorization': 'Bearer ' + token})
@@ -143,7 +156,6 @@ def get_user_info(token, realm, username):
 # ユーザのグループへの追加
 #
 def set_user_role(token, realm, uid, role_id, role_name):
-    print("set_user_role("+token+", "+realm+", "+uid+", "+ role_id+", "+role_name+")")
     r = requests.post(
         url+'/admin/realms/' + realm + '/users/' + uid + "/role-mappings/realm",
         headers={'Authorization': 'Bearer ' + token},
@@ -153,10 +165,39 @@ def set_user_role(token, realm, uid, role_id, role_name):
         print(r.text)
         quit()
 
+#
+# クライアントの作成
+#
+def create_client(token, realm, client):
+    r = requests.post(
+        url+'/admin/realms/' + realm + '/clients',
+        headers={'Authorization': 'Bearer ' + token},
+        json={'clientId': client})
+    if not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        print(r.text)
+        quit()
+
+#
+# クライアント情報の取得
+#
+def get_client(token, realm, client):
+    r = requests.get(
+        url+'/admin/realms/' + my_realm + '/clients?clientId=' + client,
+        headers={'Authorization': 'Bearer ' + token})
+    if not (200 <= r.status_code and r.status_code < 300):
+        print(r.status_code)
+        print(r.text)
+        quit()
+    return json.loads(r.text)
+
+
+
 
 def main():
     # トークン取得
     token = get_access_token()
+
     # 現在のレルム一覧を取得
     realms = get_realms(token)
     # もし今から作ろうとしているレルムがすでにあったら削除
@@ -165,27 +206,45 @@ def main():
             del_realms(token, my_realm)
     # レルム作成
     create_realm(token, my_realm)
+
     # レルムロール作成
+    role2id = {}
     for role in my_roles:
-        create_role(token, my_realm, role)
-    # ロールの情報を取得
-    role_info = get_roles_info(token, my_realm)
+        r = create_role(token, my_realm, role)
+    roles = get_roles(token, my_realm)
+    for role in roles:
+        role2id[role['name']] = role['id']
+    print(role2id)
+
     # グループ作成
     for g in my_groups:
         create_group(token, my_realm, g)
+    print(get_groups(token, my_realm))
+
     # ユーザ情報が書いてあるCSVファイルを一行ずつ読んで追加していく
     text = open(csv_file, "r", encoding="utf-8", errors="", newline="" )
     f = csv.reader(text, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
     header = next(f)
     for i, user in enumerate(f):
+        print(user)
         # ユーザ作成
         create_user(token, my_realm, user, my_groups[i%len(my_groups)])
         # ユーザ情報の取得
-        user = get_user_info(token, my_realm, user[0])
+        user = get_user(token, my_realm, user[0])
         # このユーザに追加したいロールを決めて、、
         rolename = my_roles[i%len(my_roles)]
         # ユーザにロールをマッピング
-        set_user_role(token, my_realm, user['id'], role_info[role]['id'], role)
+        set_user_role(token, my_realm, user['id'], role2id[rolename], rolename)
+
+    # レルムにクライアントを登録
+    create_client(token, my_realm, my_client)
+    # クライアントの情報を取得　※特定のクライアントの情報を取得することはできないか？
+    client = get_client(token, my_realm, my_client)
+    print(client)
+
+
 
 if __name__ == '__main__':
+    start = time.time()
     main()
+    print(time.time() - start)
